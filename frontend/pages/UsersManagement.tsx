e283c1396d0ffa7cbd8b880e31c7d139dc4bf15a
dgimenez.developer <dgimenez.developer@gmail.com>
2026-02-03 09:17:55 -0300

    el input de contraseña ahora fuerza el re-render al alternar entre mostrar y ocultar, asegurando que el tipo cambie correctamente y la contraseña se visualice al hacer click en el botón

--- frontend/pages/UsersManagement.tsx
+++ frontend/pages/UsersManagement.tsx
@@ -476,6 +476,7 @@ const UsersManagement: React.FC = () => {
                 <div className="relative">
                   <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" size={18} />
                   <input
+                    key={showPassword ? 'show' : 'hide'}
                     type={showPassword ? 'text' : 'password'}
                     value={editingUser.password || ''}
                     onChange={(e) => setEditingUser({ ...editingUser, password: e.target.value })}
