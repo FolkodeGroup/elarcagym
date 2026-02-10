431bccfa1c86eb6adb3ba86196fe9e299fee56ad
Daro <dgimenez.developer@gmail.com>
2026-02-10 05:28:48 -0300

    AllowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,39 +2,35 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-        sourcemap: false,
-        proxy: {
-          // Redirige todas las rutas de API al backend
-          '/waitlist': 'http://localhost:4000',
-          '/members': 'http://localhost:4000',
-          '/notifications': 'http://localhost:4000',
-          '/users': 'http://localhost:4000',
-          '/reservations': 'http://localhost:4000',
-          // Agrega aquí otras rutas de API si es necesario
-        },
-      },
-      preview: {
-        host: '0.0.0.0',
-        port: 4173,
-        strictPort: true,
-        allowedHosts: [
-          'elarcagym.com.ar',
-          'www.elarcagym.com.ar'
-        ]
-      },
-      build: {
-        sourcemap: false,
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    proxy: {
+      // Redirige todas las rutas de API al backend
+      '/waitlist': 'http://localhost:4000',
+      '/members': 'http://localhost:4000',
+      '/notifications': 'http://localhost:4000',
+      '/users': 'http://localhost:4000',
+      '/reservations': 'http://localhost:4000',
+      // Agrega aquí otras rutas de API si es necesario
+    },
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      'elarcagym.com.ar',
+      'www.elarcagym.com.ar'
+    ]
+  },
+  build: {
+    sourcemap: false,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
